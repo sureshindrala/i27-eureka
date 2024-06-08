7546d820a6442f7eff2a53924bf9fa30ddb8e2b8
Sumanth3420 <indralasumanth3420@gmail.com>
2024-06-08 22:49:59 +0530

    error-1

--- src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
+++ src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
@@ -1,12 +1,5 @@
 package com.lerner.eureka;
 
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
 
 
 @SpringBootTest
